1e909a08a20558bbe10bd9b2614d344d812adad6
Joshua D. Sawyer <incendiarykittens@gmail.com>
2022-01-09 22:07:35 -0600

    Added documentation

--- source.h
+++ source.h
@@ -10,4 +10,15 @@
 #include "../convert/source.h"
 #endif
 
+/** @file vluint/source.h
+    Provides a function for reading vluints described in vluint/vluint.h from a convert_source described in convert/source.h
+ */
+
 unsigned long long vluint_read_source (bool * error, convert_source * source, size_t limit);
+/**<
+   @brief Reads a vluint from the given source, consuming bytes as it reads.
+   @param error Will be set to true if an error occurs
+   @param source The source to be read
+   @param limit The maximum number of bytes to read while searching for the end of a vluint
+   @return The value of the vluint that was read, or -1 if an error occured
+*/
--- vluint.h
+++ vluint.h
@@ -8,5 +8,22 @@
 #include "../window/def.h"
 #endif
 
+/**
+   @file vluint/vluint.h
+   Provides functions for reading and writing arbitrary length unsigned integers (very-long-unsigned-integer or vluint). These integers store 7 bits per byte, so this format can be space efficient for small numbers.
+*/
+
 void vluint_write (window_unsigned_char * output, unsigned long long input);
+/**<
+   @brief This function appends an integer as a vluint to the window.
+   @param output The window to append the integer to.
+   @param input The integer to store
+*/
+
 unsigned long long vluint_read (bool * error, range_const_unsigned_char * input);
+/**<
+   @brief This function reads a vluint from the given range, consuming the bytes that it reads.
+   @param error Will be set to true if an error occurs
+   @param input The input range to be read
+   @return The value of the integer read, or -1 if an error occurred
+*/
